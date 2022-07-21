1c1bae438b98bca7f78a5e3bb7ca00bf5b42aaa2
JaeYeongSon <67821588+pereng11@users.noreply.github.com>
2022-07-21 10:50:46 +0900

    Time: 85 ms (53.10%), Space: 42.6 MB (14.69%) - LeetHub

---
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} left
+ * @param {number} right
+ * @return {ListNode}
+ */
+var reverseBetween = function(head, left, right) {
+    const storedVals = {};
+    let idx = 1;
+    let tHead = head;
+    let tLeft;
+    while(head){
+        if(idx >= left && idx <=  right){
+            const i = getReversedIdx(idx);
+            storedVals[i] = head.val;
+        } else {
+            storedVals[idx] = head.val;
+        }
+        head = head.next;
+        idx++;
+    }
+    idx = 1;
+    head = tHead;
+    while(head){
+        head.val = storedVals[idx];
+        head = head.next;
+        idx++;
+    }
+    function getReversedIdx(idx){
+        return (right - idx) + left;
+    }
+    return tHead;
+};
